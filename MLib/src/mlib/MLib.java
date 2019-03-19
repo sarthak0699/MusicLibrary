01dde2db425250ad819fbb716fd3345da4a98d60
Sarthak Singh Chauhan <38989587+sarthak0699@users.noreply.github.com>
2019-03-19 11:56:04 +0530

    Add files via upload

---
+++ MLib/src/mlib/MLib.java
@@ -0,0 +1,15 @@
+
+package mlib;
+
+
+
+public class MLib {
+
+    public static void main(String[] args) {
+       WelcomePage w=new WelcomePage();
+       w.show();
+        
+        
+    }
+    
+}
